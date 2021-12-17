f36751e57706528429a90d63d7605d0442bddaa0
suyeon-hong <bellahong95@gmail.com>
2021-12-17 11:32:22 +0900

    components 분리

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
+import './css/style.css';
+import {Logo, Menu} from './components/Layout';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <figure>
+      <Logo />
+
+      <Menu />
+
+      <Footer />
+    </figure>
   );
 }
 
