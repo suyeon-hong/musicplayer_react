cbe2a7cabb9a6f920f4acaed839ec2afcb74a37f
suyeon-hong <bellahong95@gmail.com>
2021-12-18 21:50:00 +0900

    Btns 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './css/style.css';
 import Header from './components/Header';
 import Panels from './components/Panels';
+import Btns from './components/Btns';
 import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
   const arr = ["Blizzards", "Calm", "Dusty_Road", "Escape", "Payday", "Retreat", "Seasonal", "Vespers"]
   const deg = 360/arr.length;
+  let [txt, setTxt] = useState(arr);
 
   return (
     <figure>
@@ -13,11 +16,13 @@ function App() {
 
       <section>
         {
-          arr.map((data,index)=>{
+          txt.map((data,index)=>{
             return <Panels key={index} index={index} data={data} deg={deg} />
           })
       }
       </section>
+
+      <Btns />
 
       <Footer />
     </figure>
---
+++ src/components/Btns.js
@@ -0,0 +1,14 @@
+function Btns() {
+    return(
+        <>
+            <div className="prev">
+                <span>PREV</span>
+            </div>
+            <div className="next">
+                <span>NEXT</span>
+            </div>
+        </>
+    )
+}
+
+export default Btns;
