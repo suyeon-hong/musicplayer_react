ba3c7a2eb0ebe2966e0f14ab13a3ce078e1cb096
suyeon-hong <bellahong95@gmail.com>
2021-12-18 16:02:00 +0900

    article 동적으로 반복 생성

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
-  let bgImg = {backgroundImage: `url(${process.env.PUBLIC_URL}/img/member1.jpg)`}
+  const arr = ["Blizzards", "Calm", "Dusty_Road", "Escape", "Payday", "Retreat", "Seasonal", "Vespers"]
+  const deg = 360/arr.length;
+
   return (
     <figure>
       <Header />
 
       <section>
-        <article>
-          <div className="inner">
-            <div className="pic" style={bgImg}>
-              <div className="dot"></div>
-            </div>
-            <div className="txt">
-              <h2>Title</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, itaque?</p>
-            </div>
-          </div>
-        </article>
+        {
+          arr.map((data,index)=>{
+            let bgImg = {backgroundImage: `url(${process.env.PUBLIC_URL}/img/${data}.jpg)`}
+            let rotatePanel = {transform: `rotate(${deg*index}deg) translateY(-100vh)`}
+
+            return(
+              <article key={index} style={rotatePanel}>
+                <div className="inner">
+                  <div className="pic" style={bgImg}>
+                    <div className="dot"></div>
+                  </div>
+                  <div className="txt">
+                    <h2>{data}</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, itaque?</p>
+                  </div>
+                </div>
+              </article>
+            )
+          })
+      }
       </section>
 
       <Footer />
