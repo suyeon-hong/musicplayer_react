7554fc9def5e14c91c3456874a05a9d01e98a034
suyeon-hong <bellahong95@gmail.com>
2021-12-18 22:18:54 +0900

    btns 모션 추가

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       }
       </section>
 
-      <Btns />
+      <Btns deg={deg} />
 
       <Footer />
     </figure>
--- src/components/Btns.js
+++ src/components/Btns.js
@@ -1,10 +1,24 @@
-function Btns() {
+import { useState } from "react";
+
+function Btns(props) {
+    let [index, setIndex] = useState(0);
+
+    function next(e){
+        setIndex(--index);
+        const frame = e.target.closest("figure").querySelector("section");
+        frame.style.transform = `rotate(${index*props.deg}deg)`;
+    }
+    function prev(e){
+        setIndex(++index);
+        const frame = e.target.closest("figure").querySelector("section");
+        frame.style.transform = `rotate(${index*props.deg}deg)`;
+    }
     return(
         <>
-            <div className="prev">
+            <div className="prev" onClick={e=>prev(e)}>
                 <span>PREV</span>
             </div>
-            <div className="next">
+            <div className="next" onClick={e=> next(e)}>
                 <span>NEXT</span>
             </div>
         </>
