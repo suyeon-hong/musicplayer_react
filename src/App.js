24473595764636030161465f8e24accb32b8d0b7
suyeon-hong <bellahong95@gmail.com>
2021-12-18 15:31:52 +0900

    기본 디자인 완성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './css/style.css';
-import {Logo, Menu} from './components/Layout';
+import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+  let bgImg = {backgroundImage: `url(${process.env.PUBLIC_URL}/img/member1.jpg)`}
   return (
     <figure>
-      <Logo />
+      <Header />
 
-      <Menu />
+      <section>
+        <article>
+          <div className="inner">
+            <div className="pic" style={bgImg}>
+              <div className="dot"></div>
+            </div>
+            <div className="txt">
+              <h2>Title</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, itaque?</p>
+            </div>
+          </div>
+        </article>
+      </section>
 
       <Footer />
     </figure>
