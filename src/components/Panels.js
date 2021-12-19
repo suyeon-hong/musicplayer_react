9a460aa313da163b92b71097599c68b50046039c
suyeon-hong <bellahong95@gmail.com>
2021-12-19 14:14:48 +0900

    video, 음악버튼 추가

--- src/components/Panels.js
+++ src/components/Panels.js
@@ -1,5 +1,6 @@
 function Panels(props) {
     let bgImg = {backgroundImage: `url(${process.env.PUBLIC_URL}/img/${props.data}.jpg)`}
+    let videoSrc = `url(${process.env.PUBLIC_URL}/img/${props.data}.mp3)`
     let rotatePanel = {transform: `rotate(${props.deg*props.index}deg) translateY(-100vh)`}
 
     return(
@@ -9,9 +10,15 @@ function Panels(props) {
                 <div className="dot"></div>
                 </div>
                 <div className="txt">
-                <h2>{props.data}</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, itaque?</p>
+                    <h2>{props.data}</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, itaque?</p>
+                    <div className="btns">
+                        <i className="fas fa-pause"></i>
+                        <i className="fas fa-play"></i>
+                        <i className="fas fa-redo"></i>
+                    </div>
                 </div>
+                <video src={videoSrc}></video>
             </div>
         </article>
     )
